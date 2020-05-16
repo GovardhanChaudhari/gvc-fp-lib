111d97e057a44d60bcbc85f43f67942dc48c0158
Govardhan Chaudhari <govardhanchaudhari@gmail.com>
2020-05-16 05:39:51 +0530

    maint : Remove unused code

--- src/utils/UpdateProps.ts
+++ src/utils/UpdateProps.ts
@@ -5,16 +5,10 @@ import {updateProp, updatePropIf} from "./UpdateProp";
 export const updateProps = R.curry((value, names, obj) =>  names.reduce((acc, name) => updateProp(value, name, acc), obj));
 
 // @ts-ignore
-export const updatePropsWith = R.curry((value, names, obj) => names.reduce((acc, name) => updateProp(value, name, acc), obj));
+export const updatePropsIf = R.curry((pred,value, names, obj) =>  names.reduce((acc, name) => updatePropIf(pred, value, name, acc), obj));
 
 // @ts-ignore
 export const updateObjectsProps = R.curry((value, names, objects) =>  objects.map(obj => names.reduce((acc, name) => updateProp(value, name, acc), obj)));
 
 // @ts-ignore
 export const updateObjectsPropsIf = R.curry((pred, value, names, objects) => objects.map(obj => names.reduce((acc, name) => updatePropIf(pred, value, name, acc), obj)));
-
-// @ts-ignore
-export const updateObjectsPropsWith = R.curry((value, names, objects) =>  objects.map(obj => names.reduce((acc, name) => updateProp(value, name, acc), obj)));
-
-// @ts-ignore
-export const updateObjectsPropsIfWith = R.curry((pred, value, names, objects) =>  objects.map(obj => names.reduce((acc, name) => updatePropIf(pred, value, name, acc), obj)));
